4cba9c407f59aeba33ffb4d8675629aca34ef7e5
didjsrne <hyunjae0723@gmail.com>
2022-12-02 12:32:06 +0900

    Feat : 병변탐지 앱 Inference 생성
    - utils.py : yolov5 모듈을 사용한 사물인식 함수
    - models_train/best.pt : 강아지의 피부 미란/궤양 이미지를 학습시킨 모델
    - views.py - post : input_img 입력시 사물인식 모듈을 거친 output_img base64 인코딩 형식으로 모델에 저장되고 return
    - views.py - get : 가장 최근의 InferenceModel 정보 return

---
+++ inference/utils.py
@@ -0,0 +1,28 @@
+import io
+import torch
+from PIL import Image
+import base64
+import numpy as np
+import cv2
+
+def inference(input_img):
+    
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='models_train/best.pt', force_reload=True)  # 커스텀 학습 모델 사용
+    npimg = np.fromstring(input_img, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    results = model(img, size=640)  # inference 추론
+
+    results = model(img, size=640)  # inference
+
+    results.ims  # array of original images (as np array) passed to model for inference
+    results.render()  # updates results.imgs with boxes and labels
+    for img in results.ims:  # 'JpegImageFile' -> bytes-like object
+        buffered = io.BytesIO()
+        img_base64 = Image.fromarray(img)
+        img_base64.save(buffered, format="JPEG")
+        encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
+            'utf-8')  # base64 encoded image with results
+        
+    return encoded_img_data
+            
