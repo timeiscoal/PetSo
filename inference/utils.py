c41dd16ca557aafe76ac0c4010bd26535db1469c
didjsrne <hyunjae0723@gmail.com>
2022-12-02 13:52:53 +0900

    fix : input_img 리사이즈 추가
    - input_img를 640x640 크기로 리사이즈시켜 base64 인코딩 데이터 크기 축소
    - 중복되는 변수 제거

--- inference/utils.py
+++ inference/utils.py
@@ -11,10 +11,10 @@ def inference(input_img):
     npimg = np.fromstring(input_img, np.uint8)
     decode_img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(decode_img, cv2.COLOR_BGR2RGB)
-
+    
+    img = cv2.resize(img, (640, 640))
+    
     results = model(img, size=640)  # inference 추론
-
-    results = model(img, size=640)  # inference
 
     results.ims  # array of original images (as np array) passed to model for inference
     results.render()  # updates results.imgs with boxes and labels
