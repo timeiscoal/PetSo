a409a721dbe531d8a188cd691e3ae7edbed595ab
JANG HYE RIM <fla12181@gmail.com>
2022-12-02 16:40:13 +0900

    Feat:article앱의 Article 모델 기능 추가 및 수정

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from articles import views
 
 urlpatterns = [
+    path('', views.ArticlelistView.as_view(), name='article_list_view'),
+    path('<int:article_id>/', views.ArticleView.as_view(), name='article_view'),
+    path('like/', views.LikeView.as_view(), name='like_view'),
     
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,24 +3,37 @@ from rest_framework import status
 from rest_framework.response import Response
 from articles.models import Article as ArticleModel
 from articles.models import Comment as CommentModel
-from articles.serializers import ArticleSerializer
+from articles.serializers import ArticleSerializer,ArticleListSerializer
 
 
 from django.shortcuts import render
 
-class ArticleView():
-    
-    def get(self, request):
+class ArticleView(APIView):
+    def get(self, request, article_id):
         articles = ArticleModel.objet.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def put(self, request, article_id):
+        pass 
+    
+    def delete(self, request, article_id):
+        pass
+     
+
+class ArticlelistView(APIView):
+    def get(self, request):
+        articles = ArticleModel.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        pass
+    
+         
+    
 
 
-
-
-class CommentView(APIView):
-    pass
-
-
-class CommentDetailView(APIView):
-    pass
+class LikeView(APIView):
+    def post(self,request):
+        pass
